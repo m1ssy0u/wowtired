b12335e1fce637f43059f355f6100928ab8d13d9
m1ssy0u <41233@st.ac.th>
2024-02-02 10:16:51 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import axios from "axios"
+import { useRef, useState } from "react"
+
+
+function App() {
+  const myText = useRef<HTMLInputElement>(null)
+  const [lat , setlat] = useState<number | null>(null)
+  const [long , setlong] = useState<number | null>(null)
+  setInterval(getForm ,500)
+  async function getForm(){
+    navigator.geolocation.getCurrentPosition((position)=>{
+      setlat(position.coords.latitude)
+      setlong(position.coords.longitude)
+    })
+    // console.log("getformJa")
+    const formData = await myText.current?.value
+    await axios.post("http://34.87.92.34:3333/checktext",{ text : formData ,lat,long })
+    // .then((res)=>{
+    //   console.log(res)
+    // })
+    // .catch((res)=>{
+    //   console.log(res)
+    // })
+
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="text">text</label>
+        <input ref={myText} name="text" type="text"  />
+      </form>
+    </div>
+
+  )
+}
+
+export default App
